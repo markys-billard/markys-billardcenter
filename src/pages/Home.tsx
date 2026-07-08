c16c83d9f18ce31928ed56546486638799f9f3b9
markys-billard <info@markys-billardcenter.ch>
2026-07-08 16:42:32 +0200

    Updated Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -39,8 +39,8 @@ export default function Home() {
 
   const upcomingEvents = [
     {
-      date: '27',
-      month: 'Juni',
+      date: '25',
+      month: 'July',
       titleKey: 'home.tournamentTitle',
       descriptionKey: 'home.tournamentDesc',
       link: '/turniere',
